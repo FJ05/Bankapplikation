cf9a120be69fb983d98af0adb463e10dbc43f9c8
xXToNoobForSchoolXx <simon.gottfridsson@elev.ga.lbs.se>
2022-09-27 11:55:44 +0200

    Add variables for constructor

--- BankAccount.cs
+++ BankAccount.cs
@@ -1,14 +1,16 @@
 namespace BankAccount
 {
-    class BankAccount   //Class
+    class Account   //Class
     {
         public int AcBalance { get; set; }
         public int AcNum { get; set; }
 
-        public BankAccount(int acBalance, int acNum)    // Konstruktor
+        public Account(int acBalance, int acNum)    // Konstruktor
         {
             AcBalance = acBalance;
             AcNum = acNum;
         }
+
+
     }
 }
