bf04af08a83546926602bea1ebd87d8065d63b96
FJ05 <fredrikfilip.fj@gmail.com>
2022-09-27 12:33:47 +0200

    Add test

--- BankApplication/Program.cs
+++ BankApplication/Program.cs
@@ -2,4 +2,4 @@
 
 Account account = new Account(100, "Filip Jansson", "20051116xxxx");
 
-Console.WriteLine($"");
+Console.WriteLine($"account number {account.AcNum} Customer ID {account.CustomerID} Name {account.Name} SSN {account.SSN}");
