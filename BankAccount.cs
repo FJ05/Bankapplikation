071b0e4a50606afecf3986fe43809e5b6e9c13fd
xXToNoobForSchoolXx <simon.gottfridsson@elev.ga.lbs.se>
2022-09-27 11:57:59 +0200

    Add varibels for constructor

--- BankAccount.cs
+++ BankAccount.cs
@@ -4,11 +4,16 @@ namespace BankAccount
     {
         public int AcBalance { get; set; }
         public int AcNum { get; set; }
-
-        public Account(int acBalance, int acNum)    // Konstruktor
+        public string Name { get; set; }
+        public int SSN { get; set; }
+        public int CustomerID { get; set;}
+        public Account(int acBalance, int acNum, string name, int sSN, int customerID)    // Constructor
         {
             AcBalance = acBalance;
             AcNum = acNum;
+            Name = name;
+            SSN = sSN;
+            CustomerID = customerID;
         }
 
 
