1c3116446489692c302a52885d5c6953f196cd02
xXToNoobForSchoolXx <simon.gottfridsson@elev.ga.lbs.se>
2022-09-27 11:43:52 +0200

    Fixed Classname

--- BankAccount.cs
+++ BankAccount.cs
@@ -1,11 +1,11 @@
 namespace BankAccount
 {
-    class Account   //Class
+    class BankAccount   //Class
     {
         public int AcBalance { get; set; }
         public int AcNum { get; set; }
 
-        public Account(int acBalance, int acNum)    // Konstruktor
+        public BankAccount(int acBalance, int acNum)    // Konstruktor
         {
             AcBalance = acBalance;
             AcNum = acNum;
